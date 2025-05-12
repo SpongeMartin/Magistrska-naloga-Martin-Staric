661d687c7a21b600cc71d5d1285088a916ec8ad9
Martin Starič <96232207+SpongeMartin@users.noreply.github.com>
2025-05-12 13:53:04 +0200

    improved code base pt2

--- public/init.js
+++ public/init.js
@@ -1,25 +1,10 @@
 import { GridBuffer, createBuffer } from "./utils.js";
 
-import * as WebGPU from '/WebGPU.js';
-
-import { FirstPersonController } from '/controllers/FirstPersonController.js';
-
-import {
-    Camera,
-    Material,
-    Model,
-    Node,
-    Primitive,
-    Sampler,
-    Texture,
-    Transform,
-} from '/core.js';
-
-import { loadResources } from '/loaders/resources.js';
+import { Node, Transform } from '/core.js';
 
 import { shaderInit } from "./shaders/shaderInit.js";
 
-import { UnlitRenderer } from '/renderers/UnlitRenderer.js';
+import { sceneInit } from "./scene.js"
 
 export let gridSize = 32;
 
@@ -101,57 +86,10 @@ export async function initialize(canvas) {
         minFilter: 'linear',
     });
 
-    const resources = await loadResources({
-        'mesh': new URL('/floor/floor.json', import.meta.url),
-        'image': new URL('/floor/grass.png', import.meta.url),
-    });
+    const explosion = new Node();
+    explosion.addComponent(new Transform());
 
-    const renderer = new UnlitRenderer(device, canvas, context, format);
-    await renderer.initialize();
-    
-    const scene = new Node();
-    
-    const camera = new Node();
-    camera.addComponent(new Transform({
-        translation: [0, 1, 0],
-    }));
-    camera.addComponent(new Camera());
-    camera.addComponent(new FirstPersonController(camera, canvas));
-    scene.addChild(camera);
-    
-    const floor = new Node();
-    floor.addComponent(new Transform({
-        scale: [10, 1, 10],
-    }));
-    floor.addComponent(new Model({
-        primitives: [
-            new Primitive({
-                mesh: resources.mesh,
-                material: new Material({ 
-                    baseTexture: new Texture({
-                        image: resources.image,
-                        sampler: new Sampler({
-                            minFilter: 'nearest',
-                            magFilter: 'nearest',
-                            addressModeU: 'repeat',
-                            addressModeV: 'repeat',
-                        }),
-                    }),
-                }),
-            }),
-        ],
-    }));
-    scene.addChild(floor);
-
-    function updateScene(t, dt) {
-        scene.traverse(node => {
-            for (const component of node.components) {
-                component.update?.(t, dt);
-            }
-        });
-    }
-
-    // Initializing shaders
+    const smokeTranslations = {};
     const computeShaders = {};
     const gridBuffers = {velocity: velocity, density: density, divergence: divergence, pressure: pressure, temperature: temperature};
     shaderInit(device,computeShaders);
@@ -168,6 +106,8 @@ export async function initialize(canvas) {
             canvasWidth / 8, canvasHeight / 8);
     }
 
+    let renderer, scene, camera, updateScene = sceneInit(device, canvas, context, format)
+    
     return {
         device,
         context,
---
+++ public/scene.js
@@ -0,0 +1,76 @@
+import * as WebGPU from '/WebGPU.js';
+
+import { FirstPersonController } from '/controllers/FirstPersonController.js';
+
+import {
+    Camera,
+    Material,
+    Model,
+    Node,
+    Primitive,
+    Sampler,
+    Texture,
+    Transform,
+} from '/core.js';
+
+import { loadResources } from '/loaders/resources.js';
+
+import { UnlitRenderer } from '/renderers/UnlitRenderer.js';
+
+const asyncHandler = async (renderer) => {
+    const resources = await loadResources({
+        'mesh': new URL('/floor/floor.json', import.meta.url),
+        'image': new URL('/floor/grass.png', import.meta.url),
+    });
+    await renderer.initialize();
+    return resources
+}
+
+export function sceneInit(device, canvas, context, format){
+    const renderer = new UnlitRenderer(device, canvas, context, format);
+    const resources = asyncHandler(renderer);
+    
+    const scene = new Node();
+    
+    const camera = new Node();
+    camera.addComponent(new Transform({
+        translation: [0, 1, 0],
+    }));
+    camera.addComponent(new Camera());
+    camera.addComponent(new FirstPersonController(camera, canvas));
+    scene.addChild(camera);
+    
+    const floor = new Node();
+    floor.addComponent(new Transform({
+        scale: [10, 1, 10],
+    }));
+    floor.addComponent(new Model({
+        primitives: [
+            new Primitive({
+                mesh: resources.mesh,
+                material: new Material({ 
+                    baseTexture: new Texture({
+                        image: resources.image,
+                        sampler: new Sampler({
+                            minFilter: 'nearest',
+                            magFilter: 'nearest',
+                            addressModeU: 'repeat',
+                            addressModeV: 'repeat',
+                        }),
+                    }),
+                }),
+            }),
+        ],
+    }));
+    scene.addChild(floor);
+    
+    function updateScene(t, dt) {
+        scene.traverse(node => {
+            for (const component of node.components) {
+                component.update?.(t, dt);
+            }
+        });
+    }
+
+    return renderer, scene, camera, updateScene;
+}
